003b7d748b4a14aa8d9e6a149c9a63aa247295eb
Sergey Kievskiy <knhel7@mail.ru>
2023-02-19 05:16:07 +0300

    Matchers-task: add src/__tests__/heroes.js

--- src/heroes.js
+++ src/heroes.js
@@ -1,10 +1,4 @@
 // Matchers
 
-const heroes = [
-  { name: 'мечник', health: 10 },
-  { name: 'маг', health: 100 },
-  { name: 'лучник', health: 80 },
-];
-
-const orderByHealth = () => heroes.sort((h1, h2) => h1.health - h2.health);
+const orderByHealth = (heroes) => heroes.sort((h1, h2) => h2.health - h1.health); // по убыванию
 export default orderByHealth;
